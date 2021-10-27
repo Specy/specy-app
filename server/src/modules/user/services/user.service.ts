1dbd39bde08e16a893b51cf1eb85f161da2f1101
Wiz1991 <wizoftime92@gmail.com>
2021-10-27 22:04:17 +0200

    add env example

--- server/src/modules/user/services/user.service.ts
+++ server/src/modules/user/services/user.service.ts
@@ -13,7 +13,7 @@ export class UserService {
   async findUnique(data: Prisma.UserWhereUniqueInput) {
     return this.prismaService.user.findUnique({ where: data });
   }
-  async remove(data: Prisma.UserWhereUniqueInput){
-
+  async remove(data: Prisma.UserWhereUniqueInput) {
+    return this.prismaService.user.delete({ where: data });
   }
 }
