b654b49ed8cd80e65b6e16c72bdedcb55c88751e
Wiz1991 <wizoftime92@gmail.com>
2021-11-10 09:15:54 +0100

    Axios wrapper, refresh token functionality

---
+++ client/src/utils/storage.ts
@@ -0,0 +1,15 @@
+const storagePrefix = 'specy_'
+
+const storage = {
+	getToken: () => {
+		return window.localStorage.getItem(`${storagePrefix}token`) as string
+	},
+	setToken: (token: string) => {
+		window.localStorage.setItem(`${storagePrefix}token`, token)
+	},
+	clearToken: () => {
+		window.localStorage.removeItem(`${storagePrefix}token`)
+	}
+}
+
+export default storage
