5017f6e624ef8022dbe501e3b3f66efe07251382
Specy <specy.dev@gmail.com>
2025-09-27 22:40:09 +0200

    fix mermaid

--- src/components/blog/Mermaid.svelte
+++ src/components/blog/Mermaid.svelte
@@ -1,31 +1,53 @@
-<script>
+<script lang="ts" module>
 	import { onMount } from "svelte"
 	import mermaid from "mermaid"
-	import { fade } from "svelte/transition"
-	export let height = "400px"
-	export let width = "100%"
-	export let source = ""
 
+	let amount = $state(0)
+	let initialized = $state(false)
+</script>
+
+<script>
+	import { fade } from "svelte/transition"
+
+	let { height = "400px", width = "100%", source = "" } = $props()
+
+	const id = Math.random().toString(36).substring(2, 15)
 
 	onMount(() => {
-		mermaid.initialize({
-			theme: "dark",
-			startOnLoad: false,
-		})
-		setTimeout(async () => {
-			await mermaid.run({
-				querySelector: ".mermaid",
+		if (!initialized) {
+			mermaid.initialize({
+				theme: "dark",
+				startOnLoad: false,
 			})
-		}, 100)
+			initialized = true
+		}
+		amount += 1
+		setTimeout(
+			async () => {
+				await mermaid.run({
+					querySelector: `#id_${id}`,
+				})
+			},
+			100 + amount * 100
+		)
+
+		return () => {
+			amount = Math.max(0, amount - 1)
+		}
 	})
 </script>
 
-<div class="container" style:height={height}>
-	<pre in:fade={{ delay: 1000, duration: 300 }} class="mermaid" style:height={height} style:width={width}>{source}</pre>
+<div class="container" style:height>
+	<pre
+		in:fade={{ delay: 1000, duration: 300 }}
+		class="mm"
+		style:height
+		id={`id_${id}`}
+		style:width>{source}</pre>
 </div>
 
 <style lang="scss">
-	.mermaid {
+	.mm {
 		display: flex;
 		justify-content: center;
 		align-items: center;
